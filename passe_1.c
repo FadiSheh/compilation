10793c9124f1ef2676ff606407eb32316ae53124
FadiSheh <58226655+FadiSheh@users.noreply.github.com>
2021-05-24 19:35:58 +0200

    test

--- passe_1.c
+++ passe_1.c
@@ -3,37 +3,49 @@
 
 #include "defs.h"
 #include "passe_1.h"
-
+#include "utils/miniccutils.h"
+#include "common.h"
 extern int trace_level;
 node_type dernier_type = TYPE_NONE;
 
 void analyse_passe_1(node_t root) {
-	printf("helloworld\n");
+	
 	if(!root){
 		printf("Arbre est vide\n");
 		exit(1);
 	}
-
+    printf("Nature: %s\n", node_nature2string(root->nature));
 	switch(root->nature){
 		case NODE_PROGRAM:	//
 			//initialisation contexte global vide
 			//voir fonction
 			push_global_context();
 			analyse_passe_1(root->opr[0]);
-			//analyse_passe_1(root->opr[1]);
-			//pop_context();
+			analyse_passe_1(root->opr[1]);
+			pop_context();
 			break;
 		case NODE_BLOCK: //
 			push_context();
-			analyse_passe_1(root->opr[0]);
-			analyse_passe_1(root->opr[1]);
+			if (root->opr[0]!=NULL){
+			analyse_passe_1(root->opr[0]);}
+			if (root->opr[1]!=NULL){
+			analyse_passe_1(root->opr[1]);}
 			pop_context();
 			break;
 		case NODE_IDENT: //
 			//pas d'appel à A1
 			//Deux choses en deux temps : type
 			root->type = dernier_type;
+			root->offset = env_add_element(root->ident,root);
+			if(root->offset<=0){
+				node_t tmp = get_decl_node(root->ident);
+				root->decl_node = tmp;
+				printf("Variable existe deja\n");
+
+
+			}
 			   /* TYPE_NONE,
+			}
     TYPE_INT,
     TYPE_BOOL,
     TYPE_STRING,
@@ -50,16 +62,17 @@ void analyse_passe_1(node_t root) {
 			break;
 		/*case NODE_AFFECT: //
 			//type
-			break;
+			break;*/
 		case NODE_FUNC:
+			printf("NODE FUNC\n");
 			reset_env_current_offset();
 			analyse_passe_1(root->opr[0]);
 			analyse_passe_1(root->opr[1]);
 			analyse_passe_1(root->opr[2]);
 			//apres analyse de la fonction
 			//offset
-			root->offset = get_env_current_offset();
-			break;*/
+			root->offset = env_add_element(root->ident,root);
+			break;
 		case NODE_LIST:
 			analyse_passe_1(root->opr[0]);
 			analyse_passe_1(root->opr[1]);
@@ -69,13 +82,15 @@ void analyse_passe_1(node_t root) {
 			analyse_passe_1(root->opr[1]);
 			break;
 		case NODE_DECL:
-			analyse_passe_1(root->opr[0]);
-			analyse_passe_1(root->opr[1]);
+			if (root->opr[0]!=NULL){
+			analyse_passe_1(root->opr[0]);}
+			if (root->opr[1]!=NULL){
+			analyse_passe_1(root->opr[1]);}
 			break;
 		case NODE_TYPE:
 			//pas d'appel à A1
 			dernier_type = root->type;
-			printf("type : %d\n", dernier_type);
+			printf("type node type: %d\n", dernier_type);
 			break;
 		case NODE_INTVAL:
 			//pas d'appel à A1
@@ -88,6 +103,10 @@ void analyse_passe_1(node_t root) {
 			//offset
 			root-> offset = add_string(root->str);
 			break;
+
+		case NODE_PRINT:
+			analyse_passe_1(root->opr[0]);
+			break;	
 
 		//type de tous les noeuds des opérateurs
 		/*case NODE_PLUS:
@@ -124,6 +143,7 @@ void analyse_passe_1(node_t root) {
 		default:
 			break;
 	}
+	printf("finito\n");
 }
 
 
