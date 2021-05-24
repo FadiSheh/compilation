13f3372258984de1675b8d40c74a8194c323d981
amelyy78 <58708120+amelyy78@users.noreply.github.com>
2021-05-24 20:20:34 +0200

    Add files via upload

--- passe_1.c
+++ passe_1.c
@@ -3,53 +3,62 @@
 
 #include "defs.h"
 #include "passe_1.h"
+
 #include "utils/miniccutils.h"
-#include "common.h"
 extern int trace_level;
 node_type dernier_type = TYPE_NONE;
+node_t noeud;
+int flag = 0;
 
 void analyse_passe_1(node_t root) {
-	
+	printf("helloworld\n");
 	if(!root){
 		printf("Arbre est vide\n");
-		exit(1);
+		//exit(1);
 	}
-    printf("Nature: %s\n", node_nature2string(root->nature));
+	printf("nouveau noeud \n");
 	switch(root->nature){
 		case NODE_PROGRAM:	//
 			//initialisation contexte global vide
 			//voir fonction
 			push_global_context();
+			flag = 1;
 			analyse_passe_1(root->opr[0]);
+			printf("fin des variables GLOBALES\n");
+			flag = 0;
 			analyse_passe_1(root->opr[1]);
-			pop_context();
+			printf("fin des variables LOCALES\n");
+			//pop_context();
 			break;
 		case NODE_BLOCK: //
 			push_context();
-			if (root->opr[0]!=NULL){
-			analyse_passe_1(root->opr[0]);}
-			if (root->opr[1]!=NULL){
-			analyse_passe_1(root->opr[1]);}
+			if(root->opr[0] != NULL){
+				analyse_passe_1(root->opr[0]);
+			}
+			if(root->opr[1] != NULL){
+				analyse_passe_1(root->opr[1]);
+			}
 			pop_context();
 			break;
 		case NODE_IDENT: //
 			//pas d'appel à A1
 			//Deux choses en deux temps : type
 			root->type = dernier_type;
-			root->offset = env_add_element(root->ident,root);
-			if(root->offset<=0){
-				node_t tmp = get_decl_node(root->ident);
-				root->decl_node = tmp;
-				printf("Variable existe deja\n");
-
-
+			root-> offset = env_add_element(root->ident, root);
+			if(flag){
+				//variable gloable
+				printf("c une globale\n");
+				//noeud = get_decl_node(root->ident);
 			}
-			   /* TYPE_NONE,
+			else{
+				//variable locale
+				//root-> ident = get_decl_node(root->ident);
+				root->offset = env_add_element(root->ident, root);
+				if(!get_decl_node(root->ident)){
+					root->decl_node = get_decl_node(root->ident);
+					printf("variable existe deja\n");
+				}
 			}
-    TYPE_INT,
-    TYPE_BOOL,
-    TYPE_STRING,
-    TYPE_VOID,*/
 			//offset : int32_t
 			//locale : offset de la pile
 			//globale : offset dans la section .data
@@ -60,18 +69,17 @@ void analyse_passe_1(node_t root) {
 			//adresse du noeud contenant la déclaration de la variable référencée
 			//root-> decl_node = ;
 			break;
-		/*case NODE_AFFECT: //
+		case NODE_AFFECT: //
 			//type
-			break;*/
+			break;
 		case NODE_FUNC:
-			printf("NODE FUNC\n");
 			reset_env_current_offset();
 			analyse_passe_1(root->opr[0]);
 			analyse_passe_1(root->opr[1]);
 			analyse_passe_1(root->opr[2]);
 			//apres analyse de la fonction
 			//offset
-			root->offset = env_add_element(root->ident,root);
+			root->offset = get_env_current_offset();
 			break;
 		case NODE_LIST:
 			analyse_passe_1(root->opr[0]);
@@ -82,15 +90,13 @@ void analyse_passe_1(node_t root) {
 			analyse_passe_1(root->opr[1]);
 			break;
 		case NODE_DECL:
-			if (root->opr[0]!=NULL){
-			analyse_passe_1(root->opr[0]);}
-			if (root->opr[1]!=NULL){
-			analyse_passe_1(root->opr[1]);}
+			analyse_passe_1(root->opr[0]);
+			analyse_passe_1(root->opr[1]);
 			break;
 		case NODE_TYPE:
 			//pas d'appel à A1
 			dernier_type = root->type;
-			printf("type node type: %d\n", dernier_type);
+			//printf("type : %d\n", dernier_type);
 			break;
 		case NODE_INTVAL:
 			//pas d'appel à A1
@@ -104,12 +110,8 @@ void analyse_passe_1(node_t root) {
 			root-> offset = add_string(root->str);
 			break;
 
-		case NODE_PRINT:
-			analyse_passe_1(root->opr[0]);
-			break;	
-
 		//type de tous les noeuds des opérateurs
-		/*case NODE_PLUS:
+		case NODE_PLUS:
 			//deux fils
 			//contexte?
 			analyse_passe_1(root->opr[0]);
@@ -139,11 +141,10 @@ void analyse_passe_1(node_t root) {
 		case NODE_FOR:
 			break;
 		case NODE_DOWHILE:
-			break;*/
+			break;
 		default:
 			break;
 	}
-	printf("finito\n");
 }
 
 
