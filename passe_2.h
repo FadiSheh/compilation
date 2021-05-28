595764272a86a97f3696d30a3b3ecc58cf6e828a
FadiSheh <58226655+FadiSheh@users.noreply.github.com>
2021-05-28 11:20:46 +0200

    update

--- passe_2.h
+++ passe_2.h
@@ -2,8 +2,12 @@
 #define _PASSE_2_H_
 
 #include "defs.h"
+#include "common.h"
+#include "utils/miniccutils.h"
 
 void gen_code_passe_2(node_t root);
+void analyse_gen(node_t node, int nops);
+void ajoutStrings(int nb);
 
 #endif
 
